693ff32b251049c0b8667b156a22b99d9e2c4dab
Kangkang-wky <wky_bayonet@163.com>
2023-10-06 13:03:08 +0000

    topk(local sort(bubble sort && ))

--- solution/17_14.cpp
+++ solution/17_14.cpp
@@ -12,10 +12,94 @@
 // 在前面工程都完工的情况下可以尝试测试科研一把双调(bit)排序, 桶排序,
 // 以及基数排序 (比较 SC 21 doctor topk)
 
+// 全局排序
 class Solution1 {
 public:
   vector<int> smallestK(vector<int> &arr, int k) {
     sort(arr.begin(), arr.end());
     return vector<int>(arr.begin(), arr.begin() + k);
+  }
+};
+
+// 局部排序
+// 结果正确, bubble 注意两个问题: 一是 i 的范围代表 top_k, 二是注意 j
+// 的范围和顺序决定 arr 数组 top_k 的那些位置在哪里
+class Solution2 {
+public:
+  // bubble_sort
+  void bubble_sort_topk(vector<int> &arr, int k) {
+    for (size_t i = 0; i < k; i++) { // 局部 k 次
+      for (size_t j = arr.size() - 1; j > i; j--) {
+        if (arr[j - 1] > arr[j]) {
+          swap(arr[j - 1], arr[j]);
+        }
+      }
+    }
+  }
+
+  vector<int> smallestK(vector<int> &arr, int k) {
+    bubble_sort_topk(arr, k);
+    return vector<int>(arr.begin(), arr.begin() + k);
+  }
+};
+
+// 局部排序
+// 快速排序算法, 比较有意思的点是对于 pivot 基准的选择
+// 换一种写法 quicksort 的写法以前写的都是填坑法, 这次换成划分法写婴喜爱
+// 将[left + 1, right]分成3个部分:
+// [left + 1, le)部分所有元素都 <= pivot
+// [le, ge]没有看到的部分
+// (ge, right]部分所有元素都 >= pivot
+class Solution {
+public:
+  int partition(vector<int> &arr, int left, int right) {
+    int pivot = left; // 基准 arr[left]
+    // [lt, gt] 还没有做处理的部分
+    int lt = left + 1;
+    int gt = right;
+    while (true) {
+      while (lt <= right && arr[lt] < arr[pivot]) {
+        lt++;
+      }
+      while (gt >= left && arr[gt] > arr[pivot]) {
+        gt--;
+      }
+      if (lt > gt) {
+        break;
+      }
+      // 交换 lt gt ++ --
+      swap(arr[lt], arr[gt]);
+      lt++;
+      gt--;
+    }
+    swap(arr[pivot], arr[gt]);
+    return gt;
+  }
+  // 快排部分递归, 只关心 top k 小的部分, 也就是说对于 topk
+  // 内部的排序我们是不关心的
+  void quicksort(vector<int> &arr, int left, int right, int k) {
+    if (left > right) {
+      return;
+    }
+    int pivot = partition(arr, left, right);
+    if (pivot == k)
+      return;
+    else if (pivot > k) {
+      quicksort(arr, left, pivot - 1, k);
+    } else {
+      quicksort(arr, pivot + 1, right, k);
+    }
+  }
+
+  vector<int> smallestK(vector<int> &arr, int k) {
+    // 边界情况特殊处理
+    if (k >= arr.size()) {
+      return arr;
+    } else if (k <= 0) {
+      return {};
+    } else {
+      quicksort(arr, 0, arr.size() - 1, k);
+      return vector<int>(arr.begin(), arr.begin() + k);
+    }
   }
 };
