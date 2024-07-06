55665c4dbd686de80a168e886aa847913f336523
Kangkang-wky <wky_bayonet@163.com>
2024-07-06 07:07:10 +0000

    add kernel

---
+++ learncpp_com/Chapter6/pow.cpp
@@ -0,0 +1,31 @@
+#include <cassert> // for assert
+#include <cstdint> // for std::int64_t
+#include <iostream>
+
+// note: exp must be non-negative
+// note: does not perform range/overflow checking, use with caution
+constexpr std::int64_t powint(std::int64_t base, int exp) {
+
+  // 防御性编程, 防止 exp < 0 时发生错误
+  assert(exp >= 0 && "powint: exp parameter has negative value");
+
+  // Handle 0 case
+  if (base == 0)
+    return (exp == 0) ? 1 : 0;
+
+  std::int64_t result{1};
+  while (exp > 0) {
+    if (exp & 1) // if exp is odd
+      result *= base;
+    exp /= 2;
+    base *= base;
+  }
+
+  return result;
+}
+
+int main() {
+  std::cout << powint(7, 12) << '\n'; // 7 to the 12th power
+
+  return 0;
+}
