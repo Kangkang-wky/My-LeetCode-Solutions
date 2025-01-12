2f62277f140f3f7bdb584ba059c9817b46542e8d
Kangkang-wky <wky_bayonet@163.com>
2025-01-12 12:28:54 +0000

    dfs combination sum

--- solution/0039.cpp
+++ solution/0039.cpp
@@ -5,33 +5,28 @@
 // 你可以按 任意顺序 返回这些组合。candidates 中的 同一个 数字可以 无限制重复被选取。
 // 如果至少一个数字的被选数量不同，则两种组合是不同的。 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
 
-
-// 背包解法
-// 无限背包即可
-// 
-
 // dfs 剪枝 回溯
 
 class Solution {
 private:
-    void dfs(int idx, vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& temp) {
+    void dfs(int idx, vector<int>& candidates, int target, vector<vector<int>>& res, vector<int>& temp) {
         if (idx == candidates.size()) { // 达到 idx 的最大值即可 return
             return;
         }
         
         if (target == 0) {              // 达到和最小的时候 emplace_back 即可
-            ans.emplace_back(temp);
+            res.emplace_back(temp);
             return;
         }
 
         // 直接跳过, 跳到下一个数的 dfs 部分
-        dfs(idx + 1, candidates, target, ans, temp);
+        dfs(idx + 1, candidates, target, res, temp);
         
-        // 选择当前数
+        // 判断还能减少 candidates[idx]
         if (target - candidates[idx] >= 0) {
             temp.emplace_back(candidates[idx]);
-            dfs(idx, candidates, target - candidates[idx], ans, temp);
-            temp.pop_back();
+            dfs(idx, candidates, target - candidates[idx], res, temp);
+            temp.pop_back();            // 回溯
         }
     }
 
