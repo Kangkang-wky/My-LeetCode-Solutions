47043b750980942b76c4b929045e134c357dbc3c
Kangkang-wky <wky_bayonet@163.com>
2025-01-18 17:22:41 +0800

    merge sort

--- solution/0912.cpp
+++ solution/0912.cpp
@@ -1,6 +1,6 @@
 #include "../include/help.h"
 
-class Solution {
+class Solution1 {
 public:
   // quicksort 找到 index 之后重新划分
   int partition(vector<int> &nums, int left, int right) {
@@ -50,4 +50,61 @@ public:
     quicksort(nums, 0, nums.size() - 1);
     return nums;
   }
+};
+
+
+class Solution2 {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        return mergeSort(nums);
+    }
+
+private:
+    int N;
+    vector<int> tmp;
+    
+    vector<int> mergeSort(vector<int>& nums) {
+        N = nums.size();
+        tmp = vector<int>(N, 0);
+        sort(nums, 0, N - 1);
+        return nums;
+    }
+
+    // 采用闭区间 [left close, right close]
+    void sort(vector<int>& nums, int low, int high) {
+        if (low == high) {
+            return; // single element no need sort
+        }
+
+        int mid = low + (high - low) / 2;
+        sort(nums, low, mid);
+        sort(nums, mid + 1, high);
+        merge(nums, low, mid, high);    // 将 [low, mid] 与 [mid + 1, high] 做 out of place 的 merge
+    }
+
+    void merge(vector<int>& nums, int low, int mid, int high) {
+        for (int k = low; k <= high; k++) {
+            tmp[k] = nums[k];           // out of place
+        }
+
+        // i = low 
+        // j = mid + 1
+        int i = low;
+        int j = mid + 1;
+        for (int p = low; p <= high; p++) {
+            if (i == mid + 1) {           // 前一个数组已经结束
+                nums[p] = tmp[j];
+                j++; 
+            } else if (j == high + 1) {   // 后一个数组已经计数
+                nums[p] = tmp[i];
+                i++;
+            } else if (tmp[i] > tmp[j]) { // 前大后小
+                nums[p] = tmp[j];
+                j++;
+            } else {
+                nums[p] = tmp[i];         // 前小后大
+                i++;
+            }
+        }
+    }
 };
