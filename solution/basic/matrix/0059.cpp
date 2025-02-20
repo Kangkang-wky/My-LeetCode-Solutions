0af136e926f8f310392c245983ce79fd9bf25ed0
Kangkang-wky <wky_bayonet@163.com>
2025-02-20 02:17:07 +0000

    add spiral_order matrix solution

--- solution/basic/matrix/0059.cpp
+++ solution/basic/matrix/0059.cpp
@@ -1,6 +1,6 @@
 #include "../include/help.h"
 
-class Solution {
+class Solution1 {
 private:
     static constexpr int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
@@ -24,6 +24,39 @@ public:
             row = row + dir[dir_index][0];
             column = column + dir[dir_index][1];
 
+        }
+        return res;
+    }
+};
+
+
+class Solution2 {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector <int> res;
+        if(matrix.empty()) return res; //若数组为空，直接返回答案
+        int up = 0; //赋值上下左右边界
+        int down = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+        while(true)
+        {   
+            // 按照四个方向分别进行遍历即可
+            for(int i = left; i <= right; ++i) res.push_back(matrix[up][i]); //向右移动直到最右
+            ++up;
+            if(++up > down) break;      //重新设定上边界，若上边界大于下边界，则遍历遍历完成，下同
+
+            for(int i = up; i <= down; ++i) res.push_back(matrix[i][right]); //向下
+            --right;
+            if(--right < left) break;   //重新设定有边界
+            
+            for(int i = right; i >= left; --i) res.push_back(matrix[down][i]); //向左
+            --down;
+            if(--down < up) break;      //重新设定下边界
+            
+            for(int i = down; i >= up; --i) res.push_back(matrix[i][left]); //向上
+            ++left;
+            if(++left > right) break;   //重新设定左边界
         }
         return res;
     }
