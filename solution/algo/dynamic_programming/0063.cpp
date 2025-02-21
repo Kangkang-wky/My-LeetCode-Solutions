21388d3b796534bf4c7e1be7e3bcda2e4d4e5fe7
Kangkang-wky <wky_bayonet@163.com>
2025-02-21 09:19:26 +0000

    add 0063 unique path dp

--- solution/algo/dynamic_programming/0063.cpp
+++ solution/algo/dynamic_programming/0063.cpp
@@ -1,18 +1,28 @@
 #include "../include/help.h"
 
+/**
+ * @brief 
+ * 
+ */
+
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector f(m + 1, vector<int>(n + 1));
-        f[0][1] = 1;
+        
+        // 初始化 dp 数组 
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // 需要注意这里处理 dp 数组初始化的细节
+        // 处理好循环顺序, 并设置成 dp[0][1] = 1
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (obstacleGrid[i][j] == 0) {
-                    f[i + 1][j + 1] = f[i][j + 1] + f[i + 1][j];
+                    dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j];
                 }
             }
         }
-        return f[m][n];            
+        return dp[m][n];            
     }
 };
