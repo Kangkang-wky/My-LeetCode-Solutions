e18f889857c5d0442abdd4de7e66485152082b27
Kangkang-wky <wky_bayonet@163.com>
2025-03-04 02:16:15 +0000

    add 0094(inorder_traversal) recursive

--- solution/data_structure/binary_tree/0094.cpp
+++ solution/data_structure/binary_tree/0094.cpp
@@ -4,25 +4,25 @@ class Solution {
 public:
 vector<int> inorderTraversal(TreeNode* root) 
     {
-        vector<int>v;
+        vector<int> res;
         stack<TreeNode*> st;
-        TreeNode* cur=root;
-        while(cur || st.size()!=0)
-        {
-            //左路节点
-            while(cur)
-            {
+        TreeNode* cur = root;
+
+
+        while(cur || st.size() != 0) {      // 当前节点为空节点 或者 栈不为空
+            while(cur) {
                 st.push(cur);
                 cur=cur->left;
             }
+
             //取栈顶
             TreeNode*stop=st.top();
             //栈里面取到一个节点时，一定是它的左子树访问完了
-             v.push_back(stop->val);
+            res.push_back(stop->val);
             st.pop();
             //左路节点1的右子树
             cur=stop->right;
         }
-        return v;
+        return res;
     }
 };
