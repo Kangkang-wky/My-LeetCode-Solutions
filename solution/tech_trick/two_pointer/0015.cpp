914d6521d196c952b342cfe6f5e5829c0b2aff6c
Kangkang-wky <wky_bayonet@163.com>
2025-03-06 15:03:12 +0000

    add 0015(three_sum) reivew two pointer

--- solution/tech_trick/two_pointer/0015.cpp
+++ solution/tech_trick/two_pointer/0015.cpp
@@ -7,7 +7,7 @@
  */
 
 
-class Solution {
+class Solution1 {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
@@ -32,6 +32,50 @@ public:
                     --third;
                 }
                 if (second == third) {    // 优化当前的搜索, 如果 b 的坐标 == c 的坐标了, b 超过 c, 剪枝
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    res.push_back({nums[first], nums[second], nums[third]});
+                }
+            }
+        }
+        return res;
+    }
+};
+
+/**
+ * @brief 三数之和：凑出 0 部分
+ *  给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k 
+ *  同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+ *
+ *  注意：答案中不可以包含重复的三元组。
+ */
+
+class Solution2 {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        
+        for (int first = 0; first < n; first++) {
+            // 数字重复直接跳过, 要求的是和为 0 且不重复的三元组
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+        
+            int third = n - 1;
+            int target = -nums[first];
+
+            for (int second = first + 1; second < n; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                
+                while (second < third && nums[second] + nums[third] > target) {
+                    third--;
+                }
+                if (second == third) {      // 如果 second 和 third 碰上意味着 不再存在对应的解
                     break;
                 }
                 if (nums[second] + nums[third] == target) {
